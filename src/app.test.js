6336b94ab295e9737a05f9d31445677efd22e158
Jack Zhang <ngshoy@hotmail.com>
2019-01-31 21:28:04 -0500

    added test cases for /ViewPoll/:pollId

--- src/app.test.js
+++ src/app.test.js
@@ -1,10 +1,13 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./app');
 const { Poll } = require('./db/models/pollModel');
 
+const id = new ObjectID();
 const pollData = {
+  "_id": id,
   "topic": "where should we go for lunch?",
   "options": [{
       "name": "Ematei "
@@ -26,13 +29,17 @@ describe('PUT /CreatePoll', () => {
     Poll.deleteMany({}).then(() => done());
   });
 
+  after(done => {
+    Poll.deleteMany({}).then(() => done());
+  })
+
   it('should create a new poll with correct data', done => {
     request(app)
     .put('/CreatePoll')
     .send(pollData)
     .expect(200)
     .expect(res => {
-      expect(res.body._id).toBeDefined();
+      expect(res.body._id).toEqual(id.toHexString());
       expect(res.body.options[0].name).toBe('Ematei');
       expect(res.body.options[0].voteCount).toBe(0);
     })
@@ -45,15 +52,62 @@ describe('PUT /CreatePoll', () => {
   });
 
   it('should not create a new poll with invalid data', done => {
-    pollData.topic = '';
+    const invalidData = { ...pollData, topic: ''};
 
     request(app)
     .put('/CreatePoll')
-    .send(pollData)
+    .send(invalidData)
     .expect(400)
     .expect(res => {
       expect(res.body._id).toBeUndefined();
       expect(res.body.name).toBe('ValidationError');
+    })
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      return done();
+    })
+  });
+});
+
+describe('GET /ViewPoll/:pollId', () => {
+  beforeEach(done => {
+    Poll.deleteMany({}).then(() => null);
+    const newPoll = new Poll(pollData);
+    newPoll.save().then(() => done());
+  });
+
+  after(done => {
+    Poll.deleteMany({}).then(() => done());
+  })
+
+  it('should find the poll with corect data', done => {
+    request(app)
+    .get(`/ViewPoll/${id.toHexString()}`)
+    .expect(200)
+    .expect(res => {
+      expect(res.body._id).toEqual(id.toHexString());
+      expect(res.body.topic).toEqual('where should we go for lunch?');
+      expect(res.body.options[0].name).toBe('Ematei');
+      expect(res.body.options[0].voteCount).toBe(0);
+    })
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      return done();
+    })
+  });
+
+  it('should not find any poll with invalid id', done => {
+    const invalidId = new ObjectID();
+
+    request(app)
+    .get(`/ViewPoll/${invalidId.toHexString()}`)
+    .expect(404)
+    .expect(res => {
+      expect(res.body._id).toBeUndefined();
     })
     .end((err, res) => {
       if (err) {
